195ae399c36eedaaaa834f292ad342497679b9ee
FacuJacob <facujacob1@gmail.com>
2023-06-22 17:36:51 -0300

    Codigo completo

--- Ejercicio 7/Ejercicio 7/Form1.vb
+++ Ejercicio 7/Ejercicio 7/Form1.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Form1
+    Private Sub cmdEjecutar_Click(sender As Object, e As EventArgs) Handles cmdEjecutar.Click
+        Dim vectora(8) As Integer
+        Dim vectorb(8) As Integer
+        For j = 1 To 8
+            vectora(j) = InputBox("Ingrese los valores del vector", "Vector(8)")
+        Next
+        For j = 1 To 4
+            vectorb(j) = vectora(j + 4)
+            vectorb(j) = vectora(j)
+        Next
 
+        lstVectorA.Items.Clear()
+        lstVectorB.Items.Clear()
+
+        For j = 1 To 8
+            lstVectorA.Items.Add(vectora(j))
+            lstVectorB.Items.Add(vectorb(j))
+        Next
+    End Sub
 End Class
